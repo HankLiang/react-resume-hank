dcf03f1f3e2b61ee678f13abc168b6cf04d985d5
梁勇 <liangyong@Hank.local>
2019-09-24 19:11:22 +0800

    更新简历信息

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import JParticles from 'jparticles';
 
 class Header extends Component {
-  render() {
+  componentDidMount() {
+    setTimeout(() => {
+      new JParticles.particle('#homeCover', {
+        color: '#25bfff',
+        lineShape: 'cube',
+        range: 2000,
+        proximity: 100,
+        // 开启视差效果
+        parallax: true
+      });
+    }, 0);
+  }
 
+  render() {
     if(this.props.data){
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
@@ -13,42 +26,47 @@ class Header extends Component {
       })
     }
 
-    return (
-      <header id="home">
+   return (
+    <header id="home">
+    <div id="homeCover" className="home-cover"></div>
 
-      <nav id="nav-wrap">
+    <nav id="nav-wrap">
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+      <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	    <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
+      <ul id="nav" className="nav">
+        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+        <li><a className="smoothscroll" href="#about">About</a></li>
+	      <li><a className="smoothscroll" href="#resume">Resume</a></li>
+        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+        {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
+        {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
+      </ul>
 
-      </nav>
+    </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
-            <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
+    <div className="row banner">
+      <div className="banner-text">
+        <div className="avatar-wrapper">
+          <div className="avatar">
+          </div>
+        </div>
+        <h2>梁勇, 百度高级前端开发工程师（上海研发中心）</h2>
+        <h3>对所有新鲜的事物都保持好奇心，热衷于技术，并且坚信科技改变世界</h3>
+        <hr />
+        <ul className="social">
+          {networks}
+        </ul>
       </div>
+    </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
+    <p className="scrolldown">
+      <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+    </p>
 
-   </header>
-    );
+  </header>
+   );
   }
 }
 
